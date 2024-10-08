497a73ffb8a1c9292d459cdd463f3743a425f63c
ranuriV <ranuri.v@papertrl.com>
2024-10-08 15:15:14 +0530

    my first commit

---
+++ cloud-native-buildpacks/src/main/java/com/example/buildpacks/cloud_native_buildpacks/GreetingController.java
@@ -0,0 +1,11 @@
+package com.example.buildpacks.cloud_native_buildpacks;
+
+
+@RestController
+public class GreetingController {
+
+    @GetMapping("/{name}")
+    public String hello(@PathVariable String name){
+        return "Welcome " + name +" to Cloud native World !!!";
+    }
+}
---
+++ cloud-native-buildpacks/src/test/java/com/example/buildpacks/cloud_native_buildpacks/CloudNativeBuildpacksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.buildpacks.cloud_native_buildpacks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudNativeBuildpacksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
