5baf54888d1630b263f2e69c83dc17732f9693bc
ranuriV <ranurivid8@gmail.com>
2024-10-08 17:16:49 +0530

    hello world app 1st commit

---
+++ hello-world/hello-world/src/main/java/com/example/hello_world/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.example.hello_world;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/") // This maps the root URL to this method
+    public String hello() {
+        return "Hello, World!";
+    }
+}
